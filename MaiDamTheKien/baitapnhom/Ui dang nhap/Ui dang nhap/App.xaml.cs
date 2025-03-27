55bed47b90216d8765f588099c81bbd48519954a
Hoangtranvn2005 <thunguyenbmt789@gmail.com>
2025-03-27 19:14:16 +0700

    Kiên đã cập nhật

---
+++ MaiDamTheKien/baitapnhom/Ui dang nhap/Ui dang nhap/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Ui_dang_nhap
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
